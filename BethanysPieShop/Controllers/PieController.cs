1fdf49dbb8d95a71d571b447febe01adacad3418
Kaio Teles <kaiolunateles@gmail.com>
2020-05-27 14:22:47 -0300

    Adding project in remote repository

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.AllPies;
+
+            pieListViewModel.CurrentCategory = "Cheese cakes";
+            return View(pieListViewModel);
+        }
+    
+    }
+}
